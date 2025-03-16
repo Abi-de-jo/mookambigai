06b4914faf05705af8e72b2940b27ce47d6ea2ce
Abi-de-jo <abish@example.com>
2025-03-16 18:35:43 +0530

    vercel

--- college-website-backend/index.js
+++ college-website-backend/index.js
@@ -21,5 +21,8 @@ app.use('/api/hod', hodRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/appointments', appointmentRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+export default app;
